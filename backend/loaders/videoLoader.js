767321c26b7307ee526b90e85c35d6b18b06bfa0
Octavio Serpe <octavio.serpe@gmail.com>
2021-11-23 00:18:54 -0300

    video loader with fetch timeout not working Co-authored-by: Manuel Rodriguez <manurodriguez@itba.edu.ar> Co-authored-by: Gonzalo Arca <goarca@itba.edu.ar>

--- backend/loaders/videoLoader.js
+++ backend/loaders/videoLoader.js
@@ -1,7 +1,43 @@
 const fetch = require("node-fetch");
 const mongoDB = require("../databases/mongo");
 
+let youtubeApiKeyIndex = 0;
+
+const YOUTUBE_API_KEYS = [
+    "AIzaSyC54bLIQkg2utR6vknEVZ8Nyz3KZlfKYis",
+    "AIzaSyADq14yoBnCpe2CM7Q2dOoedrqbDHOWGmQ",
+    "AIzaSyCyMhH7cWLxHNAdc2mBDsVVZnWjJ9gKb34",
+    "AIzaSyC9dyH41I2JGEyNnjgYoDQYLXbwtIgRYxw",
+    "AIzaSyCgHC1QFeoQQzVnMqv2qK6wiebFK8BpSdc"
+];
+
 const MAX_REQUESTS_PER_PLAYLIST = 3;
+
+const MAX_FETCH_ATTEMPTS = 3;
+
+async function fetchWithTimeout(url, timeout = 1000) {
+    let found = false;
+    let tries = 0;
+    let response;
+    while ( !found && tries < MAX_FETCH_ATTEMPTS) {
+        response = Promise.race([
+            fetch(url),
+            new Promise((_, reject) =>
+                setTimeout(() => reject(new Error('timeout')), timeout)
+            )
+        ])
+        .then(response => {
+            found = true;
+            return Promise.resolve(response);
+        })
+        .catch(err => tries++);
+    }
+
+    if (!found)
+        throw new Error("Timeout with url " + url);
+
+    return response;
+}
 
 function parseIso8601ToMillis(duration) {
     var reptms = /^PT(?:(\d+)H)?(?:(\d+)M)?(?:(\d+)S)?$/;
@@ -17,63 +53,99 @@ function parseIso8601ToMillis(duration) {
     return totalMillis;
 }
 
-function uploadVideosToDB(items) {
+async function retryFetch(url) {
+    let isRequestSuccessful = false;
+    let response;
 
-    const db = mongoDB.getDB();
+    while (!isRequestSuccessful && youtubeApiKeyIndex < YOUTUBE_API_KEYS.length) {
 
-    items.forEach(async item => {
-        const statistics = await fetch(`${process.env.BASE_PATH}/videos?part=statistics&id=${item.snippet.resourceId.videoId}&key=${process.env.YOUTUBE_API_KEY}`)
-            .then(response => response.json())
-            .then(json => {
-                const stats = json.items[0].statistics;
-                return {
-                    likes: stats.likeCount,
-                    dislikes: stats.dislikeCount,
-                    views: stats.viewCount,
-                    commentCount: stats.commentCount
-                };
-            })
-            .catch(err => console.log(err));
+        response = await fetchWithTimeout(`${url}&key=${YOUTUBE_API_KEYS[youtubeApiKeyIndex]}`);
 
-        const duration = await fetch(`${process.env.BASE_PATH}/videos?part=contentDetails&id=${item.snippet.resourceId.videoId}&key=${process.env.YOUTUBE_API_KEY}`)
-            .then(response => response.json())
-            .then(json => json.items[0].contentDetails.duration)
-            .catch(err => console.log(err));
-
-
-        const video = {
-            _id: item.snippet.resourceId.videoId,
-            channel_id: item.snippet.videoOwnerChannelId,
-            title: item.snippet.title,
-            description: item.snippet.description,
-            published_at: item.snippet.publishedAt,
-            duration: parseIso8601ToMillis(duration),
-            statistics
+        if (response.status != 200 && response.status != 403) {
+            throw `Error: HTTP code ${response.status} with URL ${url} and API key ${YOUTUBE_API_KEYS[youtubeApiKeyIndex]}`
         }
 
-        db.collection("videos").updateOne({ _id: video._id }, { $set: video }, { upsert: true });
+        isRequestSuccessful = response.status == 200;
 
-    });
-    console.log("FINISHED FETCHING VIDEOS");
+        if (!isRequestSuccessful)
+            youtubeApiKeyIndex++;
+
+
+    }
+
+    if (youtubeApiKeyIndex == YOUTUBE_API_KEYS.length) {
+        throw "Out of api keys";
+    }
+
+    return response.json();
+}
+
+async function uploadVideosToDB(items) {
+    const db = mongoDB.getDB();
+
+    let response;
+    for (item of items) {
+        try {
+            response = await retryFetch(`${process.env.BASE_PATH}/videos?part=statistics&id=${item.snippet.resourceId.videoId}`);
+            const statisticsResponse = response.items[0].statistics;
+
+            const statistics = {
+                likes: statisticsResponse.likeCount,
+                dislikes: statisticsResponse.dislikeCount,
+                views: statisticsResponse.viewCount,
+                commentCount: statisticsResponse.commentCount
+            };
+
+            response = await retryFetch(`${process.env.BASE_PATH}/videos?part=contentDetails&id=${item.snippet.resourceId.videoId}`);
+            duration = response.items[0].contentDetails.duration;
+
+            const video = {
+                _id: item.snippet.resourceId.videoId,
+                channel_id: item.snippet.videoOwnerChannelId,
+                title: item.snippet.title,
+                description: item.snippet.description,
+                published_at: item.snippet.publishedAt,
+                duration: parseIso8601ToMillis(duration),
+                statistics
+            }
+
+            db.collection("videos").updateOne({ _id: video._id }, { $set: video }, { upsert: true });
+        } catch (err) {
+            console.error({
+                description: "Uploading video " + item.snippet.resourceId.videoId,
+                errorMessage: err
+            });
+            throw err;
+        }
+    }
 }
 
 function load() {
-
     const db = mongoDB.getDB();
-    db.collection("playlistLoadingStatus").find().forEach(async (loadingStatus, err) => {
-        if (err) {
-            console.error("Error: " + err);
-        } else {
+    // const url = "https://youtube.googleapis.com/youtube/v3/videos?part=contentDetails&id=sdEUoMZhMAU"
+    // fetch(`${url}&key=${YOUTUBE_API_KEYS[youtubeApiKeyIndex+1]}`).then(resp => resp.json()).then(json => console.log(json));
+
+
+    db.collection("playlistLoadingStatus").find().toArray().then(async loadingStatusArray => {
+        for (loadingStatus of loadingStatusArray) {
+            console.log("Fetching playlist id: " + loadingStatus._id);
             for (let i = 0; i < MAX_REQUESTS_PER_PLAYLIST; i++) {
-                const response = await fetch(`${process.env.BASE_PATH}/playlistItems?part=snippet&playlistId=${loadingStatus._id}&maxResults=50&key=${process.env.YOUTUBE_API_KEY}&pageToken=${loadingStatus.nextPageToken || ""}`)
-                    .then(response => response.json())
-                    .catch(err => console.log(err));
-
-                uploadVideosToDB(response.items);
-
-                loadingStatus.nextPageToken = response.nextPageToken;
-                if (!response.nextPageToken) {
-                    break;
+                try {
+                    console.log("\tFetching page " + loadingStatus.nextPageToken)
+                    const response = await retryFetch(`${process.env.BASE_PATH}/playlistItems?part=snippet&playlistId=${loadingStatus._id}&maxResults=50&pageToken=${loadingStatus.nextPageToken || ""}`);
+                    await uploadVideosToDB(response.items);
+                    loadingStatus.nextPageToken = response.nextPageToken;
+                    if (!response.nextPageToken) {
+                        break;
+                    }
+                } catch (err) {
+                    if (err.message === "Out of api keys") {
+                        throw err;
+                    }
+                    console.error({
+                        description: "Loading videos",
+                        errorMessage: err
+                    });
                 }
             }
             if (loadingStatus.nextPageToken) {
@@ -83,151 +155,9 @@ function load() {
                 console.log("Deleting playlist id: " + loadingStatus._id);
                 db.collection("playlistLoadingStatus").deleteOne({ _id: loadingStatus._id });
             }
-
+            console.log("Finished loading playlistId: " + loadingStatus._id);
         }
     });
-
 }
-
-
-
-
-
-function getVideo() {
-    const playlistId = "UUN1hnUccO4FD5WfM7ithXaw";
-    const path = BASE_PATH + "/playlistItems?part=snippet&playlistId=" + playlistId + "&maxResults=50&key=" + YOUTUBE_API_KEY;
-    console.log(path);
-    fetch(path)
-        .then(response => response.json())
-        .then(json => json.items.forEach(item => console.log(item.snippet)))
-        .catch(err => console.log(err));
-}
-
-function getRating() {
-    const playlistId = "UUN1hnUccO4FD5WfM7ithXaw";
-    const path = BASE_PATH + "/videos?key=" + YOUTUBE_API_KEY + "&id=fQb_JaH2GQw&part=statistics";
-    console.log(path);
-    fetch(path)
-        .then(response => response.json())
-        .then(json => console.log(json.items[0].statistics))
-        .catch(err => console.log(err));
-}
-
-
-function getDuration() {
-    const playlistId = "UUN1hnUccO4FD5WfM7ithXaw";
-    const path = BASE_PATH + "/videos?key=" + YOUTUBE_API_KEY + "&id=fQb_JaH2GQw&part=contentDetails";
-    console.log(path);
-    fetch(path)
-        .then(response => response.json())
-        .then(json => console.log(json.items[0].contentDetails.duration))
-        .catch(err => console.log(err));
-}
-
-function writeCSVLine(snippet, rating) {
-    const line = `${snippet.resourceId.videoId},${snippet.channelId},${snippet.title},"${snippet.description}",${snippet.publishedAt}`
-    fs.writeFile("../datasets/videos.json", line, err => console.error(err))
-}
-
-const rating = {
-    "kind": "youtube#videoListResponse",
-    "etag": "G5-mVJ5Knt2je2O_smbYPhgqwEE",
-    "items": [
-        {
-            "kind": "youtube#video",
-            "etag": "MHlK8tqWEC78CmcnvzyRD_hK4Gk",
-            "id": "fQb_JaH2GQw",
-            "statistics": {
-                "viewCount": "3358890",
-                "likeCount": "35924",
-                "dislikeCount": "902",
-                "favoriteCount": "0",
-                "commentCount": "995"
-            }
-        }
-    ],
-    "pageInfo": {
-        "totalResults": 1,
-        "resultsPerPage": 1
-    }
-}
-
-const duration = {
-    "kind": "youtube#videoListResponse",
-    "etag": "t-hVC7-1yUcGuWbc9D4s3ivhPuA",
-    "items": [
-        {
-            "kind": "youtube#video",
-            "etag": "bv3g_k62GvMnNdkhNiyi4nSJ8XY",
-            "id": "3eSfCeTV2qI",
-            "contentDetails": {
-                "duration": "PT2M49S",
-                "dimension": "2d",
-                "definition": "hd",
-                "caption": "false",
-                "licensedContent": true,
-                "contentRating": {},
-                "projection": "rectangular"
-            }
-        }
-    ],
-    "pageInfo": {
-        "totalResults": 1,
-        "resultsPerPage": 1
-    }
-}
-
-const snippet = {
-    publishedAt: '2017-10-16T22:14:26Z',
-    channelId: 'UCN1hnUccO4FD5WfM7ithXaw',
-    title: 'Maroon 5, Julia Michaels - Help Me Out ft. Julia Michaels (Lyric Video)',
-    description: '"RED PILL BLUES” is out now.\n' +
-        'http://smarturl.it/RedPillBlues \n' +
-        '\n' +
-        'For more, visit: \n' +
-        'https://www.facebook.com/maroon5 \n' +
-        'https://twitter.com/maroon5 \n' +
-        'https://www.instagram.com/maroon5 \n' +
-        '\n' +
-        'Sign up for updates: http://smarturl.it/Maroon5.News/n' +
-        '\n' +
-        'Music video by Maroon 5, Julia Michaels performing Help Me Out. (C) 2017 Interscope Records\n' +
-        '\n' +
-        'http://vevo.ly/tgNbbC',
-    thumbnails: {
-        default: {
-            url: 'https://i.ytimg.com/vi/JA2JdSP7wyg/default.jpg',
-            width: 120,
-            height: 90
-        },
-        medium: {
-            url: 'https://i.ytimg.com/vi/JA2JdSP7wyg/mqdefault.jpg',
-            width: 320,
-            height: 180
-        },
-        high: {
-            url: 'https://i.ytimg.com/vi/JA2JdSP7wyg/hqdefault.jpg',
-            width: 480,
-            height: 360
-        },
-        standard: {
-            url: 'https://i.ytimg.com/vi/JA2JdSP7wyg/sddefault.jpg',
-            width: 640,
-            height: 480
-        },
-        maxres: {
-            url: 'https://i.ytimg.com/vi/JA2JdSP7wyg/maxresdefault.jpg',
-            width: 1280,
-            height: 720
-        }
-    },
-    channelTitle: 'Maroon5VEVO',
-    playlistId: 'UUN1hnUccO4FD5WfM7ithXaw',
-    position: 49,
-    resourceId: { kind: 'youtube#video', videoId: 'JA2JdSP7wyg' },
-    videoOwnerChannelTitle: 'Maroon5VEVO',
-    videoOwnerChannelId: 'UCN1hnUccO4FD5WfM7ithXaw'
-}
-
 
 module.exports = load;
