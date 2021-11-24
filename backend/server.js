37d16f97d0b578df10a8d3362ae4c0d212e31728
Gonzalo Arca <goarca@itba.edu.ar>
2021-11-23 22:47:33 -0300

    installed material ui - frontend init

--- backend/server.js
+++ backend/server.js
@@ -24,22 +24,22 @@ require("./routes/testRoutes")(app);
 
 // este es el endpoint "/..." (donde cae todo lo que no matchee)
 app.use((req, res, next) => {
-	const error = new Error("Endpoint does not exists");
-	error.status = 404;
-	next(error);
-})
+  const error = new Error("Endpoint does not exists");
+  error.status = 404;
+  next(error);
+});
 
 app.use((error, req, res, next) => {
-	const status = error.status || 500;
-	const message = error.message || "Internal server error";
-	res.status(status).json({ message });
+  const status = error.status || 500;
+  const message = error.message || "Internal server error";
+  res.status(status).json({ message });
 });
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-	console.log(`SERVER LISTENING ON PORT ${port}`);
-})
+  console.log(`SERVER LISTENING ON PORT ${port}`);
+});
 
 if (process.env.NODE_ENV == "load") {
 	mongoDB.connect(mongoDBLoader).then(() => {
@@ -60,7 +60,7 @@ else {
 	mongoDB.connect(() => { });
 }
 
-process.on('SIGINT', function () {
-	// some other closing procedures go here
-	process.exit(0);
+process.on("SIGINT", function () {
+  // some other closing procedures go here
+  process.exit(0);
 });
