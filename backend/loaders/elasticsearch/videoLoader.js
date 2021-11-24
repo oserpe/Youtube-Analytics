07710fa5232cc663314f74ed651e2c819b5131de
Gonzalo Arca <goarca@itba.edu.ar>
2021-11-23 23:28:02 -0300

    elasticsearch loader implemented
    Co-authored-by: Manuel Rodriguez <manurodriguez@itba.edu.ar>
    Co-authored-by: Octavio Serpe <oserpe@itba.edu.ar>

--- backend/loaders/elasticsearch/videoLoader.js
+++ backend/loaders/elasticsearch/videoLoader.js
@@ -2,64 +2,72 @@ const mongoDB = require("../../databases/mongo");
 const elasticDB = require("../../databases/elasticsearch");
 
 async function load() {
-	const mongoClient = mongoDB.getDB();
-	const elasticClient = elasticDB.getDB();
-	try {
-		await elasticClient.indices.putMapping({
-			index: 'videos',
-			body: {
-				properties: {
-					video_id: { "type": "string" },
-					title: { "type": "string" },
-					description: { "type": "string" },
-					channel_id: { "type": "string" },
-					published_at: {
-						"type": "date",
-						"format": "EEE MMM dd HH:mm:ss Z yyyy"
-					},
-				}
-			}
+  const mongoClient = mongoDB.getDB();
+  const elasticClient = elasticDB.getDB();
+  try {
+    await elasticClient.indices.create({
+      index: "videos",
+      body: {
+        mappings: {
+          video: {
+            properties: {
+              video_id: { type: "text" },
+              title: { type: "text" },
+              description: { type: "text" },
+              channel_id: { type: "text" },
+              published_at: {
+                type: "date",
+                format: "YYYY-MM-DD'T'HH:mm:ssZ",
+              },
+            },
+          },
+        },
+      },
+    });
+  } catch (err) {
+    console.error(err);
+  }
 
-		});
-	}
-	catch (err) {
-		console.error(err);
-	}
+  console.log("After index creation");
 
-	console.log("After index creation")
+  const videos = await mongoClient
+    .collection("videos")
+    .find()
+    .toArray()
+    .then(async (videos) => {
+      for (const video of videos) {
+        try {
+          await elasticClient.index({
+            index: "videos",
+            body: {
+              video_id: video._id,
+              title: video.title,
+              description: video.description,
+              channel_id: video.channel_id,
+              published_at: video.published_at,
+            },
+          });
+        } catch (err) {
+          console.error(err);
+        }
+      }
+    });
 
-	// const videos = await mongoClient.collection("videos").find().toArray();
+  // We need to force an index refresh at this point, otherwise we will not
+  // get any result in the consequent search
+  await elasticClient.indices.refresh({ index: "videos" });
 
-	// for (video of videos) {
-	// 	console.log(video);
-	// 	await elasticClient.index({
-	// 		index: "videos",
+  // Let's search!
+  const { body } = await elasticClient.search({
+    index: "videos",
+    body: {
+      query: {
+        match: { title: "CFK" },
+      },
+    },
+  });
 
-	// 		body: {
-	// 			"properties": {
-	// 				"video_id": video._id,
-	// 				"title": video.title,
-	// 				"description": video.description,
-	// 				"channel_id": video.channel_id,
-	// 			}
-	// 		}
-	// 	});
-	// }
-	// // We need to force an index refresh at this point, otherwise we will not
-	// // get any result in the consequent search
-	// await elasticClient.indices.refresh({ index: 'videos' })
-
-	// // Let's search!
-	// const { body } = await client.search({
-	// 	index: 'videos',
-	// 	body: {
-	// 		query: {
-	// 			match: { title: 'CFK' }
-	// 		}
-	// 	}
-	// })
-
-	// console.log(body.hits.hits)
+  console.log(body.hits.hits);
 }
 
 module.exports = load;
--- backend/server.js
+++ backend/server.js
@@ -24,43 +24,40 @@ require("./routes/testRoutes")(app);
 
 // este es el endpoint "/..." (donde cae todo lo que no matchee)
 app.use((req, res, next) => {
-	const error = new Error("Endpoint does not exists");
-	error.status = 404;
-	next(error);
-})
+  const error = new Error("Endpoint does not exists");
+  error.status = 404;
+  next(error);
+});
 
 app.use((error, req, res, next) => {
-	const status = error.status || 500;
-	const message = error.message || "Internal server error";
-	res.status(status).json({ message });
+  const status = error.status || 500;
+  const message = error.message || "Internal server error";
+  res.status(status).json({ message });
 });
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-	console.log(`SERVER LISTENING ON PORT ${port}`);
-})
+  console.log(`SERVER LISTENING ON PORT ${port}`);
+});
 
 if (process.env.NODE_ENV == "load") {
-	mongoDB.connect(mongoDBLoader).then(() => {
-		elasticsearch.connect(elasticsearchLoader).then(() => {
-			neo4j.connect(neo4jLoader);
-		});
-	});
-}
-else if (process.env.NODE_ENV == "load-neo") {
-	mongoDB.connect(() => { })
-		.then(() => neo4j.connect(neo4jLoader));
-}
-else if (process.env.NODE_ENV == "load-elastic") {
-	mongoDB.connect(() => { })
-		.then(() => elasticsearch.connect(elasticsearchLoader));
-}
-else {
-	mongoDB.connect(() => { });
+  mongoDB.connect(mongoDBLoader).then(() => {
+    elasticsearch.connect(elasticsearchLoader).then(() => {
+      neo4j.connect(neo4jLoader);
+    });
+  });
+} else if (process.env.NODE_ENV == "load-neo") {
+  mongoDB.connect(() => {}).then(() => neo4j.connect(neo4jLoader));
+} else if (process.env.NODE_ENV == "load-elastic") {
+  mongoDB
+    .connect(() => {})
+    .then(() => elasticsearch.connect(elasticsearchLoader));
+} else {
+  mongoDB.connect(() => {});
 }
 
-process.on('SIGINT', function () {
-	// some other closing procedures go here
-	process.exit(0);
+process.on("SIGINT", function () {
+  // some other closing procedures go here
+  process.exit(0);
 });
